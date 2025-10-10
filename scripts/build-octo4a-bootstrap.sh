4f6ff9580f5af6400eec790b3a33d75ad57984df
Filip Grzywok <f.grzywok@gmail.com>
2025-10-10 17:51:17 +0200

    fix: Downgrade alpine due to newer gcc

--- scripts/build-octo4a-bootstrap.sh
+++ scripts/build-octo4a-bootstrap.sh
@@ -5,7 +5,7 @@ set -eu
 
 # alpine packages required to install octoprint
 readonly O4A_PKGS='alpine-keys apk-tools gcc python3 py3-pip py3-netifaces libffi-dev python3-dev rust cargo musl-dev curl git linux-headers dropbear p7zip bash unzip ffmpeg ttyd'
-readonly ALPINE_BRANCH='3.22'
+readonly ALPINE_BRANCH='3.20'
 
 case "$ARCH" in
 "aarch64")
